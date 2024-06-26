59743dc005ef698525344e02da456953df3d68f5
L7ONEL <lionelardusso4730@gmail.com>
2024-06-26 18:38:39 -0300

    commit

--- src/Componentes/FiltrarPersonas.jsx
+++ src/Componentes/FiltrarPersonas.jsx
@@ -4,44 +4,26 @@ export default class FiltrarPersonas extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dni: "",
-      nombre: "",
-      apellido: ""
+      dni: ""
     }
   }
 
   render() {
     return (
       <div style={{display: 'flex', flexDirection: 'row'}}>
-
         <h4>
           DNI: <input 
-                  type="number" 
+                  type="text" 
                   value={this.state.dni} 
                   onChange={(e) => this.setState({dni:e.target.value})}
                 />
-        </h4>
-        <h4 style={{marginLeft: '10px'}}>
-          Nombre: <input 
-                    type="text" 
-                    value={this.state.nombre} 
-                    onChange={(e) => this.setState({nombre:e.target.value})}
-                  />
-        </h4>
-        <h4 style={{marginLeft: '10px'}}>
-          Apellido: <input 
-                      type="text" 
-                      value={this.state.apellido} 
-                      onChange={(e) => this.setState({apellido:e.target.value})}
-                    />
         </h4>
 
         <button 
           className='Boton' 
           style={{backgroundColor: 'gray', marginLeft: '5px'}}
-          onClick={() => this.props.extraerPersonas(this.props.token, this.state.dni, this.state.nombre, this.state.apellido)}
+          onClick={() => this.props.extraerPersonas(this.props.token, this.state.dni)}
         >Filtrar Persona/s</button>
-
       </div>
     )
   }
